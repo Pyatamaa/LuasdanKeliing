1a5997ab80ce36e3ee63992430baa006941dd7d0
Hp <nurpratama001@gmail.com>
2021-10-29 06:29:00 -0700

    menambahkan LdanKlkg.py

--- LdanKlkg.py
+++ LdanKlkg.py
@@ -1,1 +1,7 @@
-#menghitung end
+import math 
+r = float(input("Jari-jarinya adalah : "))
+luas = math.pi*r*r
+keliling = 2*math.pi*r
+
+print("luas Lingkaran = ", format(luas,'.2f'))
+print("keliling Lingkaran = ", format(keliling,'.2f'))
